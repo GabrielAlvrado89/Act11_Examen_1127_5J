205ea90d9c2257f9c8610042d38a3b566b129721
Gabriel Alvarado <a.22308051281127@cbtis128.edu.mx>
2024-09-27 00:27:23 -0600

    Update claseProveedor1127.py

--- claseProveedor1127.py
+++ claseProveedor1127.py
@@ -1,23 +1,33 @@
 class proveedor1127:
     id_proveedores1127 = 0
-    Nombre_Producto1127 = ""
+    Producto1127 = ""
     Cantidad1127 = 0
     Sucursal1127 = ""
     Num_Telefono1127 = 0
     Correo1127 = ""
     Horario1127 = 0
+
     def mostrarDatos1127(self):
-        print(f" Nombre producto: {self.Diccionario_Cantidad}")
+        print(f" Id del proveedor: {self.id_proveedores1127}")
+        print(f" Nombre del producto: {self.Producto1127}")
+        print(f" Cantidad: {self.Cantidad1127}")
+        print(f" Sucursal: {self.Sucursal1127}")
+        print(f" Num. de telefono: {self.Num_Telefono1127}")
+        print(f" Correo: {self.Correo1127}")
+        print(f" Horario: {self.Horario1127}")
     def Lista_proveedor1127(self):
         id_proveedor = [12567, 17272, 82828, 87654, 848543, 65435, 76554]
+        print("Id de los proveedores: ")
         print(id_proveedor) 
         for prov in id_proveedor:
             print (prov)
     def Tupla_Correo(self):
+        print("Correos electronicos:")
         Correo = ("Jorge@gmail.com", "Jhon@gmail.com", "Mark@gmail.com", "noob@gmail.com", "Aldo@gmail.com", "Sequi@gmail.com", "Rick@gmail.com" )
         for correo in Correo:
             print(correo)
     def Diccionario_Cantidad(self):
+        print("Cantidad de los productos: ")
         Cantidad =	{
     "De la rosa": 32,
     "Nucita": 56,
@@ -31,16 +41,26 @@ class proveedor1127:
         for x,y in Cantidad.items():
             print(x,y)
     def altas1127(self):
-            print("La operacion se realizo correctamente")
+        print("\nLa operacion altas se realizo correctamente")
     def bajas1127(self):
-        print("La operacion se realizo correctamente")
+        print("La operacion bajas se realizo correctamente\n")
 
 objeto = proveedor1127()
+
+objeto.id_proveedores1127=2345
+objeto.Producto1127="Paletas"
+objeto.Cantidad1127=32
+objeto.Sucursal1127="Sucursal No 214"
+objeto.Num_Telefono1127 = 6564871285
+objeto.Correo1127="GAbriel@gmail.com"
+objeto.Horario1127="12:30 am a 6:00pm"
+
+print("\nDatos de lista")
+objeto.Lista_proveedor1127()
+print("\nDatos de tupla")
+objeto.Tupla_Correo()
+print("\nDatos de diccionario")
+objeto.Diccionario_Cantidad()
+
 objeto.altas1127()
 objeto.bajas1127()
-print("")
-objeto.Lista_proveedor1127()
-print("")
-objeto.Tupla_Correo()
-print("")
-objeto.Diccionario_Cantidad()
